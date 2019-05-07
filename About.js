3141af7a8bf4425ae3f167a733a335cae10ce2f3
yardenez <yardenez@post.bgu.ac.il>
2019-05-07 04:17:32 +0300

    added About

---
+++ About.js
@@ -0,0 +1,21 @@
+function closeDialog(){
+    document.getElementById('About').style.display="none";
+    document.getElementById('About').close();
+}
+
+document.addEventListener('keyup', function(e) {
+    if (e.code == "Escape") {//check
+        closeDialog();
+    }
+});
+
+$(document).ready(function(){
+    var modal= document.getElementById("About");
+    window.onclick =function (event){
+        if(event.target === modal) {
+            modal.style.display = "none";
+            modal.close();
+        }
+    };
+});
+
